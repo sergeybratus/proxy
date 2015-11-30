0494020e860b9f1571453caf714143f4b1bc9a83
jadamcrain <jadamcrain@gmail.com>
2015-11-30 09:42:59 -0800

    parser now logs pretty printed frames at DEBUG level

--- libs/src/dnp3cpp/DNP3Parser.cpp
+++ libs/src/dnp3cpp/DNP3Parser.cpp
@@ -1,5 +1,7 @@
 #include "dnp3cpp/DNP3Parser.h"
 
+#include <easylogging++.h>
+
 namespace proxy { namespace  dnp3 {
 
 DNP3Parser::DNP3Parser(IParserCallbacks& callbacks) :
@@ -43,6 +45,11 @@ DNP3_Callbacks DNP3Parser::GetCallbacks()
 
 void DNP3Parser::OnLinkFrame(void *env, const DNP3_Frame *frame, const uint8_t *buf, size_t len)
 {
+    // TODO - correct log level?
+    char* output = dnp3_format_frame(frame);
+    LOG(TRACE) << output;
+    free(output);
+
     if(frame->func == DNP3_UNCONFIRMED_USER_DATA ||
        frame->func == DNP3_CONFIRMED_USER_DATA) {
         return;
@@ -68,12 +75,24 @@ void DNP3Parser::OnAppInvalid(void *env, DNP3_ParseError e)
 
 void DNP3Parser::OnAppFragment(void *env, const DNP3_Fragment *fragment, const uint8_t *buf, size_t len)
 {
+    // TODO - correct log level?
+    char* output = dnp3_format_fragment(fragment);
+    LOG(TRACE) << output;
+    free(output);
+
     reinterpret_cast<DNP3Parser*>(env)->m_callbacks.QueueWrite(RSlice(buf, len));
 }
 
 void DNP3Parser::OnLogError(void *env, const char *fmt, ...)
 {
+    char output[80];
 
+    va_list args;
+    va_start(args, fmt);
+    snprintf(output, 80, fmt, args);
+    va_end(args);
+
+    LOG(ERROR) << "DNP3 parser error: " << output;
 }
 
 }}
